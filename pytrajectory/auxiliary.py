fix broadcasting problem in auxiliary.sym2num_vectorfield(...)

pytrajectory/auxiliary.py
@@ -296,6 +296,11 @@ def sym2num_vectorfield(f_sym, x_sym, u_sym, p_sym, vectorized=False, cse=False,
         raise TypeError(str(sym_type))
 
     if vectorized:
+
+        # TODO: Check and clean up
+        # All this code seems to be obsolete
+        # we now use explicit broadcasting of the result (see below)
+
         # in order to make the numeric function vectorized
         # we have to check if the symbolic expression contains
         # constant numbers as a single expression
@@ -348,12 +353,14 @@ def sym2num_vectorfield(f_sym, x_sym, u_sym, p_sym, vectorized=False, cse=False,
 
     if sym_dim == 1:
         def f_num(x, u, p):
-            xu = stack((x, u, p))
-            return np.array(_f_num(*xu))
+            xup = stack((x, u, p))
+            raw = _f_num(*xup)  # list of arrays of potentially different length (1 or n)
+            return np.array(np.broadcast_arrays(*raw))
     else:
         def f_num(x, u, p):
-            xu = stack((x, u, p))
-            return _f_num(*xu)
+            xup = stack((x, u, p))
+            raw = _f_num(*xup)  # list of arrays of potentially different length (1 or n)
+            return np.array(np.broadcast_arrays(*raw))
 
     return f_num
 
@@ -374,6 +381,7 @@ def check_expression(expr):
     else:
         if not isinstance(expr, sp.Basic) and not isinstance(expr, sp.Matrix):
             raise TypeError("Not a sympy expression!")
+
 
 def make_cse_eval_function(input_args, replacement_pairs, ret_filter=None, namespace=None):
     """

pytrajectory/collocation.py
@@ -253,7 +253,6 @@ class CollocationSystem(object):
 
                 # now, this 2d array should be rearranged to a flattened vector
                 # the constraint-values should be handled separately (they are not part of ff(x)-xdot)
-                IPS()
                 F1 = F0[:n_states, :]
                 C = F0[n_states:, :]
 
@@ -262,8 +261,6 @@ class CollocationSystem(object):
                 # first index changing fastest, and the last index changing slowest.
                 F = F1.ravel(order='F').take(take_indices, axis=0)[:, None]
 
-                
-                
                 # calculate xdot:
                 dX = MC.Mdx.dot(c)[:,None] + MC.Mdx_abs
                 # dX has shape (ns*nc) x 1
@@ -694,7 +691,7 @@ class CollocationSystem(object):
                         try:
                             free_coeffs_guess = s_new.interpolate(s_old.f, m0=df0, mn=dfn)
                         except TypeError as e:
-                            IPS()
+                            # IPS()
                             raise e
                         guess = np.hstack((guess, free_coeffs_guess))
 

pytrajectory/solver.py
@@ -243,7 +243,7 @@ class Solver:
                     # this should might be caused by large values for xs
                     # but it should have been catched above
                     logging.warn("rho = nan (should not happen)")
-                    IPS()
+                    # IPS()
                     raise NanError()
 
                 if rho < 0:
@@ -340,7 +340,7 @@ class Solver:
         # self.par = np.array(self.sol[-len(self.par):]) # self.itemindex
 
     def log_break_reasons(self, flag):
-    # TODO: write docstring
+        # TODO: write docstring
         if not flag:
             return
 