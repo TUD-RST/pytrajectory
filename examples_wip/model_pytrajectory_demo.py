257211dbc7f97af13a0b3e98eddc53f8a79f0fbe
cknoll <CarstenKnoll@gmx.de>
2017-07-07 21:13:25 +0200

    two models which are currently under investigation

--- examples_wip/model_pytrajectory_demo.py
+++ examples_wip/model_pytrajectory_demo.py
@@ -6,22 +6,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
 import pickle
-from IPython import embed as IPS
+from ipHelp import IPS
 #http://sourceforge.net/p/python-control/wiki/Download/
 import control.matlab as controlm
 # sys.path.append('/Volumes/work/workspaces/PYTHON/mymodules')
 import symbtools as st
 
-from pytrajectory import ControlSystem
+from pytrajectory import TransitionProblem, penalty_expression
 from numpy import pi
 
+
+msg = """
+
+Dieses Skript demonstriert ein paar neue Features von pytrajectory.
+
+- penalty_constraints
+- plot intermediate results
+- (reference-solution) (noch nicht fertig)
+
+
+Wegen eines pickle-Fehlers habe ich hier noch das alte Modell geladen.
+Siehe Quell-Code.
+
+
+OK?
+"""
+# raw_input(msg)
+
+
+
 # Daten des Modells laden
-
-print("\n"*2 + "Wegen eines pickle-Fehlers habe ich hier noch das alte Modell geladen."
-      " Siehe Quell-Code\n\n")
-raw_input("OK?")
-
-
 fname = "pickles/model_alt.pcl"
 with open(fname, "rb") as pfile:
     pdict = pickle.load(pfile)
@@ -46,9 +60,10 @@ q3dd_fnc = sp.lambdify([q1, q2, q3, q4, q1d, q2d, q3d, q4d, a], q3dd_expr, 'symp
 q4dd_fnc = sp.lambdify([q1, q2, q3, q4, q1d, q2d, q3d, q4d, a], q4dd_expr, 'sympy')
 
 
+# nicht lineares partiell linarisiertes Modell
 
-# nicht lineares partiell linarisiertes Modell 
-def model_rhs(state, u):
+limit = .7
+def model_rhs(state, u, evalconstr=True):
     x1, x2, x3, x4, x5, x6, x7, x8 = state # q1, q2, q3, q4, q1d, q2d, q3d, q4d
     stell, = u
     x1d = x5
@@ -61,7 +76,25 @@ def model_rhs(state, u):
     x7d = q3dd_fnc(x1, x2, x3, x4, x5, x6, x7, x8, stell)
     x8d = q4dd_fnc(x1, x2, x3, x4, x5, x6, x7, x8, stell)
 
-    return np.array([x1d, x2d, x3d, x4d, x5d, x6d, x7d, x8d])
+    res = [x1d, x2d, x3d, x4d, x5d, x6d, x7d, x8d]
+    if evalconstr:
+        # penalty expression:
+        pe = penalty_expression(x1, -10, 10)  # egal
+        pe += penalty_expression(stell, -limit, limit)  # Eingangsbeschränkung
+        res.append(pe)
+
+    return np.array(res)
+
+
+# Visualisierung der penalty-constraints
+if 0:
+    u1 = sp.Symbol('u1')
+    pe_fnc = sp.lambdify(u1, penalty_expression(u1, -limit, limit, m=20, scale=100), modules="numpy")
+    kk = np.linspace(-3, 3, 1e3)
+    plt.plot(kk, pe_fnc(kk))
+    plt.show()
+    raise SystemExit
+
 
 Ta = 0.0
 Tb = 3.2
@@ -70,7 +103,8 @@ ua = 0.0
 ub = 0.0
 
 xa = [pi, pi, pi, 0.0, 0.0, 0.0, 0.0, 0.0]
-xb = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+xb = [pi, pi, pi, 1.0, 0.0, 0.0, 0.0, 0.0]
+
 
 # S = ControlSystem(model_rhs, Ta, Tb, xa, xb, ua, ub)
 # state, u = S.solve()
@@ -78,19 +112,19 @@ from pytrajectory import log
 
 log.console_handler.setLevel(10)
 
-# now we create our Trajectory object and alter some method parameters via the keyword arguments sx=640,
-S = ControlSystem(model_rhs, Ta, Tb, xa, xb, ua, ub, constraints=None,
-                  eps=4e-1, su=30, kx=2, use_chains=False,
-                  use_std_approach=False,
-                  sol_steps=200,
-                  show_intermediate_result=True)
+# now we create our Trajectory object and alter some method parameters via the keyword arguments,
+S = TransitionProblem(model_rhs, Ta, Tb, xa, xb, ua, ub, constraints=None,
+                      eps=1e-1, su=30, kx=2, use_chains=False,
+                      first_guess={'seed': 3},
+                      use_std_approach=False,
+                      sol_steps=200,
+                      show_ir=True)
 
 # dt_sim=0.004
 # time to run the iteration
 x, u = S.solve()
 print "successed!"
 
-IPS()
 
 # N = 100
 # for i in range(1, N):
@@ -188,7 +222,8 @@ xmin = 0
 xmax = 0
 A.set_limits(xlim=(xmin - 1.5, xmax + 1.5), ylim=(-2.0,2.0))
 
-# if 'plot' in sys.argv:
-# A.show(t=S.b)
-A.animate()
-A.save('images/TriplePendulum' + str(Tb) + '.gif')
+if 'plot' in sys.argv:
+    A.show(t=S.b)
+if 0:
+    A.animate()
+    A.save('images/TriplePendulum' + str(Tb) + '.gif')
