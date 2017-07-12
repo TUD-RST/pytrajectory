712b15f871fbd3945a77252b36dd9d7c2bf32d29
cknoll <CarstenKnoll@gmx.de>
2017-07-12 11:41:02 +0200

    reorganizing double-integrator-unittests

--- test/test_examples.py
+++ test/test_examples.py
@@ -10,52 +10,59 @@ import inspect
 import pytest
 
 import pytrajectory
+from pytrajectory import TransitionProblem
 
 
+# define the vectorfield
+def f(x, u):
+    x1, x2 = x
+    u1, = u
+
+    ff = [x2, u1]
+
+    return ff
+
+
+# system state boundary values for a = 0.0 [s] and b = 2.0 [s]
+xa = [0.0, 0.0]
+xb = [1.0, 0.0]
+
+
+# noinspection PyPep8Naming
 class TestExamples(object):
-    # get the path of the current file
-    pth = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    
-    examples_dir = os.path.join(pth, "test_examples")
 
-    # now we test, if we can get the example scripts
-    test_example_path_failed = True
-    with open(os.path.join(examples_dir, 'di_pure.py')) as f:
-        f.close()
-        test_example_path_failed = False
+    def test_di_integrator_pure(self):
+        S1 = TransitionProblem(f, a=0.0, b=2.0, xa=xa, xb=xb, ua=0, ub=0,
+                               show_ir=False,
+                               use_chains=False)
+        S1.solve()
+        assert S1.reached_accuracy
 
-    reason = "Cannot get example scripts!"
+    def test_di_constraint_x2_projective(self):
+        con = {'x2': [-0.1, 0.65]}
+        S1 = TransitionProblem(f, a=0.0, b=2.0, xa=xa, xb=xb, ua=0, ub=0, constraints=con,
+                               show_ir=False,
+                               use_chains=False)
+        S1.solve()
+        assert S1.reached_accuracy
 
-    @staticmethod
-    def assert_reached_accuracy(loc):
-        for value in loc.values():
-            if isinstance(value, pytrajectory.system.TransitionProblem):
-                assert value.reached_accuracy
+    def test_di_con_u1_projective_integrator(self):
+        con = {'u1': [-1.2, 1.2]}
+        S1 = TransitionProblem(f, a=0.0, b=2.0, xa=xa, xb=xb, ua=0, ub=0, constraints=con,
+                               show_ir=False,
+                               use_chains=False)
+        S1.solve()
+        assert S1.reached_accuracy
 
-    @pytest.mark.skipif(test_example_path_failed, reason=reason)
-    def test_duble_integrator_pure(self):
-
-        script = os.path.join(self.examples_dir, 'di_pure.py')
-        d = dict(locals(), **globals())
-        execfile(script, d, d)
-        self.assert_reached_accuracy(locals())
-
-    @pytest.mark.skipif(test_example_path_failed, reason=reason)
-    def test_duble_integrator_constraint_x1_projective(self):
-
-        script = os.path.join(self.examples_dir, 'di_contraint_x1_projective.py')
-        d = dict(locals(), **globals())
-        execfile(script, d, d)
-        self.assert_reached_accuracy(locals())
-
-    @pytest.mark.skipif(test_example_path_failed, reason=reason)
-    def test_duble_integrator_constraint_u1_projective(self):
-
-        script = os.path.join(self.examples_dir, 'di_contraint_u1_projective.py')
-        d = dict(locals(), **globals())
-        execfile(script, d, d)
-        self.assert_reached_accuracy(locals())
-
+    def test_di_con_u1_x2_projective_integrator(self):
+        con = {'u1': [-1.3, 1.3], 'x2': [-.1, .8],}
+        S1 = TransitionProblem(f, a=0.0, b=2.0, xa=xa, xb=xb, ua=0, ub=0, constraints=con,
+                               show_ir=True,
+                               use_chains=False)
+        S1.solve()
+        assert S1.reached_accuracy
 
 if __name__ == "__main__":
     print("\n"*2 + r"   please run py.test -s %filename.py"+ "\n")
+    # or: py.test -s --pdbcls=IPython.terminal.debugger:TerminalPdb %filename
+
--- test/test_examples/README.md
+++
@@ -1,1 +0,0 @@
-This directory contains examples which are used for unittests (and are probably not interesting enough for the user to be 'official examples').
