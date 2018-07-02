2d44d145e925f8c536e3250c1ac98f5a45bcf568
Carsten Knoll <carsten.knoll@posteo.de>
2018-07-02 14:47:30 +0200

    aux.multisolve_arglist: adapt expected test-results to new calling interface

--- pytrajectory/__init__.py
+++ pytrajectory/__init__.py
@@ -26,7 +26,7 @@ from distutils.version import LooseVersion
 __version__ = '1.5.1'
 
 # +++ Marker-Comment: next line will be changed by pre-commit-hook +++
-__date__ = "2018-06-11 10:07:35"
+__date__ = "2018-07-02 14:47:31"
 
 
 # check versions of dependencies
